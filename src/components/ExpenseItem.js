20a62b5ef6ad4915c63255f05d3b23c4741b1574
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-07 22:26:23 +0530

    EcpenseItems Display by hard code

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem() {
+    return (
+        <h3>Expense Item!
+            <p>Food Rs 10</p>
+            <p>Petrol Rs 100</p>
+            <p>Movies Rs 200</p>
+        </h3>
+    )
+}
+export default ExpenseItem;
