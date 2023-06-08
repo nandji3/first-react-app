b023fa4770941a4918002e4544c727777088b18d
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-08 18:02:10 +0530

    Understand the Passing data as a props and calling function component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expense = [
+    { id: 'e', LocationOfExpenditure: 'Car Insurance ', amount: 264.5, date: new Date(2023, 2, 27) },
+    { id: 'e', LocationOfExpenditure: 'Food ', amount: 950, date: new Date(2023, 2, 27) },
+    { id: 'e', LocationOfExpenditure: 'Travel ', amount: 675, date: new Date(2023, 2, 27) },
+  ];
+
   return (
     <div className="App">
-      <ExpenseItem />
-      <ExpenseItem />
-      <ExpenseItem />
+      {expense.map((exp, i) => {
+        return (<ExpenseItem key={i} title={exp.LocationOfExpenditure} amount={exp.amount} date={exp.date} />)
+      })}
     </div >
   );
 }
