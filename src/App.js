d296963cebaca3188bb7d5606918ec672d6a29c1
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-08 20:11:58 +0530

    Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   const expense = [
-    { id: 'e', LocationOfExpenditure: 'Car Insurance ', amount: 264.5, date: new Date(2023, 2, 27) },
-    { id: 'e', LocationOfExpenditure: 'Food ', amount: 950, date: new Date(2023, 2, 27) },
-    { id: 'e', LocationOfExpenditure: 'Travel ', amount: 675, date: new Date(2023, 2, 27) },
+    { id: 'e1', LocationOfExpenditure: 'Car Insurance ', amount: 264.5, date: new Date(2023, 7, 27) },
+    { id: 'e2', LocationOfExpenditure: 'Food ', amount: 950, date: new Date(2023, 2, 27) },
+    { id: 'e3', LocationOfExpenditure: 'Travel ', amount: 675, date: new Date(2023, 5, 27) },
   ];
 
   return (
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,13 @@
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
     return (
         <div key={props.key} className="expense-item">
-            <div>{props.date.toISOString()}</div>
-            <div className="expense-item__description">
-                <h2>{props.title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
+            <ExpenseDate date={props.date} />
+            <ExpenseDetails title={props.title} amount={props.amount} />
         </div>
     )
 }
