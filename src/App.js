94fbc790742b040bbad043b3cd33a14a20ec2011
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-13 21:00:52 +0530

    Add delete button and Its functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import './App.css';
-import ExpenseItem from './components/Expenses/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 import Card from './components/UI/Card';
 
 const App = () => {
-  const expense = [
-    { id: 'e1', title: 'Car Insurance ', LocationOfExpenditure: 'Bhopal', amount: 264.5, date: new Date(2023, 7, 27) },
-    { id: 'e2', title: 'Food ', LocationOfExpenditure: 'Indore', amount: 950, date: new Date(2023, 2, 27) },
-    { id: 'e3', title: 'Travel ', LocationOfExpenditure: 'Gwalior', amount: 675, date: new Date(2023, 5, 27) },
-  ];
 
   return (
     <Card className="App">
-      {expense.map((exp, i) => {
-        return (<ExpenseItem key={i} title={exp.title} LocationOfExpenditure={exp.LocationOfExpenditure} amount={exp.amount} date={exp.date} />)
-      })}
+      <Expenses />
     </Card >
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,13 +3,26 @@ import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
 
+
 const ExpenseItem = (props) => {
+    const style = {
+        fontSize: "1rem",
+        fontWeight: "bold",
+        color: "white",
+        backgroundColor: "red",
+        border: "1px solid white",
+        padding: "0.5rem",
+        borderRadius: "8px",
+        margin: "20px"
+    }
 
     return (
         <Card key={props.key} className="expense-item">
             <ExpenseDate date={props.date} />
             <ExpenseDetails title={props.title} LocationOfExpenditure={props.LocationOfExpenditure} amount={props.amount} />
-        </Card>
+            <button onClick={(e) => e.target.parentElement.remove()} style={style}>Delete</button>
+        </Card >
     )
 }
 export default ExpenseItem;
+
