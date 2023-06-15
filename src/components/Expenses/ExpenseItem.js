3dec03498f89168d411975749b9d91e9055c8e52
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-15 15:27:38 +0530

    Add button to change expense to $100.

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,9 +2,14 @@ import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import './ExpenseItem.css';
 import Card from '../UI/Card';
+import { useState } from 'react';
 
 
 const ExpenseItem = (props) => {
+
+    const [title, setTitle] = useState(props.title)
+    const [expense, setExpense] = useState(props.amount)
+
     const style = {
         fontSize: "1rem",
         fontWeight: "bold",
@@ -13,14 +18,27 @@ const ExpenseItem = (props) => {
         border: "1px solid white",
         padding: "0.5rem",
         borderRadius: "8px",
-        margin: "20px"
+        marginTop: "20px",
+        marginBottom: "20px",
+        marginLeft: "20px"
+    }
+
+    const changeTitle = () => {
+        setTitle("Title Updated");
+        console.log(title);
+    }
+
+    const changeExpense = () => {
+        setExpense("100");
     }
 
     return (
         <Card key={props.key} className="expense-item">
             <ExpenseDate date={props.date} />
-            <ExpenseDetails title={props.title} LocationOfExpenditure={props.LocationOfExpenditure} amount={props.amount} />
+            <ExpenseDetails title={title} LocationOfExpenditure={props.LocationOfExpenditure} amount={expense} />
+            <button onClick={changeExpense} style={{ ...style, backgroundColor: "#1793bd" }}>Change Expense</button>
             <button onClick={(e) => e.target.parentElement.remove()} style={style}>Delete</button>
+            <button onClick={changeTitle} style={{ ...style, backgroundColor: "#1793bd" }}>Change Title</button>
         </Card >
     )
 }
