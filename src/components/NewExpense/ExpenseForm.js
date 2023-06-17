7745fb2c79788545f7eb4a150501e5970243d315
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-17 09:29:15 +0530

    Multiple State converted into single state by 1st way

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,44 +4,29 @@ import { useState } from "react";
 
 const ExpenseForm = () => {
 
-    const [enteredTitle, setEnteredTitle] = useState("");
-    const [enteredLocation, setEnteredLocation] = useState("");
-    const [enteredAmount, setEnteredAmount] = useState("");
-    const [enteredDate, setEnteredDate] = useState("");
-    const [expenseTodo, setExpenseTodo] = useState([]);
-
-    const allExpenses = () => {
-        var newExpenses = {
-            title: enteredTitle,
-            location: enteredLocation,
-            amount: enteredAmount,
-            date: enteredDate,
-            status: false,
-        };
-        setExpenseTodo([...expenseTodo, newExpenses]);
-    };
+    const [userInput, setUserInput] = useState({ enterdTitle: "", enterdLocation: "", enterdAmount: "", enterdDate: "" })
 
     return (
         <form action="">
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <lable>Title</lable>
-                    <input type="text" onChange={(e) => setEnteredTitle(e.target.value)} placeholder="Enter Expense Title" />
+                    <input type="text" onChange={(e) => setUserInput({ ...userInput, enterdTitle: e.target.value })} placeholder="Enter Expense Title" />
                 </div>
                 <div className="new-expense__control">
                     <lable>Location</lable>
-                    <input type="text" onChange={(e) => setEnteredLocation(e.target.value)} placeholder="Enter Expense Location" />
+                    <input type="text" onChange={(e) => setUserInput({ ...userInput, enterdLocation: e.target.value })} placeholder="Enter Expense Location" />
                 </div>
                 <div className="new-expense__control">
                     <lable>Amount</lable>
-                    <input type="number" onChange={(e) => setEnteredAmount(e.target.value)} min="0.01" step="0.01" placeholder="Enter Expense Amount" />
+                    <input type="number" onChange={(e) => setUserInput({ ...userInput, enterdAmount: e.target.value })} min="0.01" step="0.01" placeholder="Enter Expense Amount" />
                 </div>
                 <div className="new-expense__control">
                     <lable>Date</lable>
-                    <input type="date" onChange={(e) => setEnteredDate(e.target.value)} min="2019-01-01" step="2023-06-16" placeholder="Enter Expense Date" />
+                    <input type="date" onChange={(e) => setUserInput({ ...userInput, enterdDate: e.target.value })} min="2019-01-01" step="2023-06-16" placeholder="Enter Expense Date" />
                 </div>
                 <div className="new-expense__actions">
-                    <button type="submit" onClick={allExpenses}>Add Expenses</button>
+                    <button type="submit" >Add Expenses</button>
                 </div>
             </div>
         </form>
