d0a00c5cf785f11daaa8857f57374d4d2f4377c6
nandji3@gmail.com <115684770+nandji3@users.noreply.github.com>
2023-06-20 15:38:08 +0530

    Added some css in expenseContent  h4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,14 +16,20 @@ const Expenses = ({ expenses }) => {
         return (expense.date.getFullYear().toString() === filteredYear);
     })
 
+    let style = { color: "Red", marginLeft: "15px", backgroundColor: "white", border: "2px solid yellow", width: "21%" }
+
+    let expensesContent = <h4 style={style}>No Expenses Found</h4>
+    if (filteredExpenses.length > 0) {
+        expensesContent = filteredExpenses.map((exp, i) => {
+            return (<ExpenseItem key={exp.id} title={exp.title} location={exp.location} amount={exp.amount} date={exp.date} />)
+        })
+    }
+
     return (
         <div>
             <Card className='expenses'>
                 <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {filteredExpenses.length === 0 && <h4>No Expenses Found</h4>}
-                {filteredExpenses.length > 0 && filteredExpenses.map((exp, i) => {
-                    return (<ExpenseItem key={exp.id} title={exp.title} location={exp.location} amount={exp.amount} date={exp.date} />)
-                })}
+                {expensesContent}
             </Card >
         </div>
     );
